e23effa43e52122d36f7d7a999d961a48424cfc5
solitdio079 <solitdio079@gmail.com>
2025-04-19 21:15:28 +0300

    Finish sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(firstNumber,secondNumber) {
+    let end = secondNumber 
+    let start = firstNumber
+    if(end < 0 || start < 0) return "ERROR"
+    if(end % 1 !== 0 || start % 1 !== 0) return "ERROR"
+    if(typeof end !== "number" || typeof start !== "number" ) return "ERROR"
+
+    if(firstNumber > secondNumber){
+        end = firstNumber
+        start = secondNumber
+    }
+    let totalSum = 0
+
+    for(let i = start; i<=end;i++){
+        totalSum += i
+    }
+    return totalSum
 
 };
 
