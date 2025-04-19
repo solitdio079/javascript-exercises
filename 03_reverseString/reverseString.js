559a7b446cd4d7b128b649909423de00f96f411b
solitdio079 <solitdio079@gmail.com>
2025-04-19 18:01:34 +0300

    Finish reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(toBeReversed) {
+    const correctOrderArray = toBeReversed.split("")
+    const reverseArray = []
+    for(let i = correctOrderArray.length; i > 0; i--){
+        reverseArray.push(correctOrderArray[i-1])
+    }
+
+    return reverseArray.join("")
 
 };
 
