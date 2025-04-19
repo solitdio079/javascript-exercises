718a81e41052d966270fb8ff08eac31b021d7d26
solitdio079 <solitdio079@gmail.com>
2025-04-19 21:00:02 +0300

    Finish removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
 const removeFromArray = function() {
+    if(arguments.length === 0) {
+        return "Please, provide at least two arguments"
+    }
+    const inputArray = arguments[0]
+    const args = [...arguments].slice(1)
+   
+
+    //Remmove elements from array
+    const resultArray = inputArray.filter(item => !args.includes(item))
+
+    return resultArray
 };
 
 // Do not edit below this line
