9c0c40cbc0c99101bb2da8fe39dabcd51800ad89
solitdio079 <solitdio079@gmail.com>
2025-04-30 03:50:49 +0300

    Complete calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,38 @@
-const add = function() {
+const add = function(a,b) {
+  return a+b
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  return a-b
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  return arr.reduce((acc,curr) => acc+=curr, 0)
+
 	
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((acc,curr) => acc*=curr, 1)
 
 };
 
-const power = function() {
+const power = function(a,b) {
+  return a**b
 	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if(num === 0) return 1
+  if(num<0) return 
+  const numArr = []
+  for(let i = 1; i<= num;i++){
+    numArr.push(i)
+
+  }
+  return multiply(numArr)
 	
 };
 
