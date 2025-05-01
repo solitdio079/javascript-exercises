cb9c24af2af8945b746ff7a028dc40d7881981fc
solitdio079 <solitdio079@gmail.com>
2025-05-01 23:46:30 +0300

    Solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Step 1: Split the string
+    const strSplitArray = str.split("")
+    //Filter the array get get rid of the special characters
+    const strSplitArrayFiltered = strSplitArray.filter(item => !([";"," ",",","!",".","?"].includes(item)))
+    // reverse the array
+    const strSplitArrayFilteredReversed = strSplitArrayFiltered.slice().reverse()
+
+    //console.log(strSplitArrayFiltered.join("").toLowerCase(),strSplitArrayFilteredReversed.join("").toLowerCase())
+
+    return strSplitArrayFiltered.join("").toLowerCase() === strSplitArrayFilteredReversed.join("").toLowerCase()
+
 
 };
 
