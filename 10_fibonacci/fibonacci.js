3affc2544926b896f547b3a780439f34a7a0207e
solitdio079 <solitdio079@gmail.com>
2025-05-02 09:26:01 +0300

    Solve Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    if(position < 0) return "OOPS"
+    if(+position === 0) return 0
+    const fibonacciStart = [1]
+    for(let i = 1;i<position;i++){
+        if(i-2 < 0){
+            fibonacciStart[i] = fibonacciStart[i-1] + (0)
+        }else{
+            fibonacciStart[i] = fibonacciStart[i-1] + fibonacciStart[i-2]
+        }
+       
+    }
+    //console.log(first)
+    return fibonacciStart[position-1]
 
 };
 
