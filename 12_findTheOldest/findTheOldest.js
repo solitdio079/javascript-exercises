f255b703dfbe76121470f4b21c69ef09428a9fee
solitdio079 <solitdio079@gmail.com>
2025-05-02 09:57:40 +0300

    Solve Oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((acc,curr) => {
+        // check for acc
+        if(!acc) return curr
+        //check if year of death exists
+        if(acc.yearOfDeath === undefined) {
+            acc.yearOfDeath = (new Date()).getFullYear()
+        }
+        if(curr.yearOfDeath === undefined) {
+            curr.yearOfDeath = (new Date()).getFullYear()
+        }
+        // GET the ages
+        const accAge = acc.yearOfDeath - acc.yearOfBirth
+        const currAge = curr.yearOfDeath - curr.yearOfBirth
+
+        if(accAge > currAge){
+            return acc
+        }else{
+            return curr
+        }
+    })
 
 };
 
