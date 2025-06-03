9b60fca3aed1604cf3fab6857012b07b9ec53e76
solitdio079 <solitdio079@gmail.com>
2025-06-04 02:31:42 +0300

    Webpack

---
+++ JSPractice/one.js
@@ -0,0 +1,3 @@
+// one.js
+export default "Hello, Odinite!";
+export const farewell = "Bye bye, Odinite!";
---
+++ JSPractice/two.js
@@ -0,0 +1,5 @@
+// two.js
+import  greeting from "./one.js";
+
+console.log(greeting); // "Hello, Odinite!"
+//console.log(farewell); // "Bye bye, Odinite!"
